44e66325ba74c3d0ef4f5d1806c358ead2f5b48b
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2020-05-17 15:25:24 +0530

    added this file

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Path to save files
+
+PROJECT_PATH = '/home/aritra/analyzeninvest-projects/stock-research/'
+EXCEL_PATH            = PROJECT_PATH + 'save_valuation.xlsx'
+COMPANY_LISTED_INDIA  = PROJECT_PATH + 'Equity-India-filtered.csv'
+COMPANY_LISTED_US     = PROJECT_PATH + 'Equity-US-filtered.csv'
+IMAGE_PATH            = PROJECT_PATH + 'images/'
+
+# Static Variables for Valuation
+
+MAX_TAX_RATE                = 0.35
+LONG_TERM_GROWTH_RATE       = 0.025
+BOND_RATE_10Y_US            = 0.0188
+FD_RATE_INDIA               = 0.07
+AVG_RETURN_OF_MARKET_US     = 0.10
+AVG_RETURN_OF_MARKET_INDIA  = 0.12
+DISCOUNT_FACTOR_INDIA       = 0.15
+DISCOUNT_FACTOR_US          = 0.075
+YEARS_TILL_STABLE_GROWTH    = 10
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from stock_valuation import stock_research
+
+
+stock_research('NAUKRI', True)
+
+
